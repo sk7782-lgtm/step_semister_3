db5c13276a440855406a103944c2796b9052e8fc
sk7782-lgtm <sk7782@srmist.edu.in>
2026-08-22 09:53:26 +0530

    Create program1.java

---
+++ program1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class PrimeNumber{
+  public static void main(String []args){
+    Scanner input = new Scanner(System.in);
+    int number.input.nextInt();
+    boolean isprime=true;
+    if(number<2){
+      isprime=False;
+    }
+    else{
+      for(int i=2;i<number;i++);
+      if(number%1==0){
+        isprime=false;
+        break;
+      }
+    }
+  }
+  System.ot.println("the given number:"+ number + "a Prime number?" +isPrime);
